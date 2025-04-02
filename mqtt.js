d8836a48889e3ed715b4de87288efa4f9f2f8eab
dj7nt <joerg@dj7nt.de>
2025-04-02 20:04:15 +0200

    smaller

--- mqtt.js
+++ mqtt.js
@@ -2,14 +2,11 @@
 const config = require("./config.js");		// Configfile einbinden
 var mqttserver=config.mqttserver.host;		// mqtt-host aus json im configfile holen (ginge auch direkt)
 var mqtt = require('mqtt');			// mqtt-module einbinden
-var dateFormat = require('dateformat');		// date-format-module einbinden
-var topica='#';					// variable in der das topic gehalten wird
 const path = require('path');
 const express = require('express');
 const app = express();		// http-express framework laden (macht routing, etc.)
 const http = require('http').Server(app);	// http-server module laden
 const io = require('socket.io')(http);		// socket.io einbinden
-var client=new Array();				// Haelt die einzelnen mqtt-clients je (browser-)client
 
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 
@@ -45,19 +42,18 @@ mqttC.on('message', function (topic, message) {	// Handler, wenn mqtt-message ko
 	}
 	if (topic.startsWith('wavelog/qso/logged')) {
 		tobrowser=parse_qso_msg(msg.content);
-		io.emit("mqtt",tobrowser);				// und raus an den Browser (nur fuer DIESES Socket, nicht fuer alle Clients) damit
+		io.emit("mqtt",tobrowser);				// und raus an den Browser damit
 		console.log(topic+' / QSO from: '+tobrowser.station_call+' with '+tobrowser.call+' in Mode: '+tobrowser.mode+' at '+tobrowser.qso_time);
 	} else {
 		tobrowser=parse_cat_msg(topic,msg.content);
-		io.emit("cat",tobrowser);				// und raus an den Browser (nur fuer DIESES Socket, nicht fuer alle Clients) damit
+		io.emit("cat",tobrowser);				// und raus an den Browser damit
 		console.log(topic+' / CAT for User '+tobrowser.user_id+' at '+tobrowser.qrg+' in Mode '+tobrowser.mode);
 	}
-	// socket.emit("mqtt",parse_msg(msg));				// und raus an den Browser (nur fuer DIESES Socket, nicht fuer alle Clients) damit
 });
 
 io.on('connection', (socket) => {			// Neue socket.io Connection?
 	console.log(socket.id + " connected");		// Debug
-	socket.on("disconnect", (reason) => {		// Socket.io Client gone? Dann mqtt fuer diesen Client wieder schliessen
+	socket.on("disconnect", (reason) => {		
 		console.log(socket.id + " disconnected");
 	});
 
